81b8a84a06fbecbf2c9d8ff0dc24b842bea98816
ming yang <ming@ming.email>
2017-11-04 20:56:55 -0500

    skip

--- ytskip.js
+++ ytskip.js
@@ -1,7 +1,22 @@
 
+var timesToSkip = [[0,10], [30,40], [60,65]];
+
+
 $( document ).ready(function() {
     var video = $(".html5-main-video").first()[0];
     console.log(video);
-    video.currentTime = 45;
-    console.log("Skipped to 45");
+
+    video.ontimeupdate = function() {
+
+        timesToSkip.forEach(function(time){
+            console.log(time);
+            var cTime = video.currentTime;            
+            if(cTime > time[0] && cTime < time[1]){
+                console.log(cTime);
+                video.currentTime = time[1];
+            }
+        }); 
+    };
+    
+    
 });
