4aea863ef654a4424e8a4b6b4a213cf78b2a0d24
peakay <peakaylopolis@gmail.com>
2017-11-04 21:55:43 -0400

    Update ytskip.js
    added highlight function

--- ytskip.js
+++ ytskip.js
@@ -5,3 +5,22 @@ $( document ).ready(function() {
     video.currentTime = video.getCurrentTime() + 10;
     console.log("Skipped ahead 10 seconds");
 });
+
+var pixelLength = $('.ytp-progress-list').width();
+
+var timeinseconds = $('.html5-main-video')[0].duration;
+console.log('time in seconds ' + timeinseconds);
+var highlight = function(startTime, length){
+	var skipPercent = length/timeinseconds;
+
+	var startPercent = startTime/timeinseconds;
+	var left = startPercent*pixelLength;
+
+	var width = skipPercent*pixelLength;
+	console.log('left ' + left);
+	console.log('width ' + width);
+	$('.ytp-progress-list').append('<div class="ytp-play-progress testclass ytp-swatch-background-color" style="left:' + left +'px; width: '+width+'px; background-color: blue;"></div>');
+
+
+
+};
