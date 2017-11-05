3075f5b863ddf8b36ad48bfc8350651271dbad94
vapaavetehinen <vapaavetehinen@users.noreply.github.com>
2017-11-04 21:37:19 -0400

    retooled for skipping ahead instead of to set value

--- ytskip.js
+++ ytskip.js
@@ -2,6 +2,6 @@
 $( document ).ready(function() {
     var video = $(".html5-main-video").first()[0];
     console.log(video);
-    video.currentTime = 45;
-    console.log("Skipped to 45");
+    video.currentTime = video.getCurrentTime() + 10;
+    console.log("Skipped ahead 10 seconds");
 });
