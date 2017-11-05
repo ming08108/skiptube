e660dd8d2fcb6ea40f273ed306b2d47d19610677
ming yang <ming@ming.email>
2017-11-04 20:31:13 -0500

    ytskip

--- ytskip.js
+++ ytskip.js
@@ -1,2 +1,7 @@
 
-console.log("HII");
+$( document ).ready(function() {
+    var video = $(".html5-main-video").first()[0];
+    console.log(video);
+    video.currentTime = 45;
+    console.log("Skipped to 45");
+});
